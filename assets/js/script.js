955950528e890e38156be9f4d2851430e89c7ebf
inayatullahaaxioned <inayatullaha@axioned.com>
2022-11-25 19:23:26 +0530

    js added for sliders in webpage and header sticky

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,87 @@
 /* Author: 
-
+Inayatullah
 */
+"use strict"
+//header sticky and background changes 
+const header = document.querySelector('.header');
+window.addEventListener('scroll', function() {
+    if(this.scrollY > header.offsetHeight + 250 ){
+        header.classList.add('sticky');
+    }else{
+        header.classList.remove('sticky');
+    }
+})
+//slider for gallery section
+$('.gallery-list').slick({
+    dots: true,
+    infinite: false,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+//slider for testimonial section 
+$('.testimonial-list').slick({
+    dots: true,
+    infinite: false,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
 
 
